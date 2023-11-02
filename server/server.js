cd8f94287d0f01a65f1c7f52745a6e441dc67183
RORY MCCONNELL <skunkbentley@hotmail.com>
2023-11-02 14:56:14 +0000

    server 1st

---
+++ server/server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const PORT = process.env.PORT || 8080;
+const app = express();
+app.use(cors());
+const axios = require("axios");
+
+// add your endpoints here
+app.get("/", (_, response) => response.json("Root route for translatetim."));
+
+//searchquery- property-value (.query refers to everything after the ? in the url)
+// express.request.query returns our object with property and value
+
+app.get("/translate", async (request, response) => {
+  // const word = request.query.word;
+  // const from = request.query.from;
+  // const to = request.query.to;
+  //  OR and BETTER
+  // destructure all the properties from request.query into variables
+  const { word, from, to } = request.query;
+
+  const API = `https://api.mymemory.translated.net/get?q=${word}&langpair=${from}|${to}`;
+  const res = await axios.get(API);
+
+  const wrangledData = {
+    translation: res.data.responseData.translatedText,
+    match: res.data.responseData.match,
+  };
+
+  response.json(wrangledData);
+});
+
+app.listen(PORT, () => console.log(`App is running PORT ${PORT}`));
