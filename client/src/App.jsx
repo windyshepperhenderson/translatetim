e032055cbcaab09d3cbd6a306132cf60363c1ed7
RORY MCCONNELL <skunkbentley@hotmail.com>
2023-11-02 16:48:43 +0000

    updated output field

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,12 +5,12 @@ import axios from "axios";
 function App() {
   //store our 'from' and 'to' languages in state
   const [from, setFrom] = useState("ar");
-  const [to, setTo] = useState("es");
+  const [to, setTo] = useState("ar");
 
   //store the word we want to translate in state
   //translation returns an object (of data) so thats what we are using in brackets
   const [word, setWord] = useState("");
-  const [translation, setTranslation] = useState({});
+  const [translation, setTranslation] = useState("");
 
   //on change function for the input of the word we want to translate
 
@@ -19,7 +19,7 @@ function App() {
     event.preventDefault();
     const API = `http://localhost:8080/translate?word=${word}&from=${from}&to=${to}`;
     const res = await axios.get(API);
-    setTranslation(res.data);
+    setTranslation(res.data.translation);
   }
 
   return (
@@ -47,7 +47,7 @@ function App() {
             <option value="es">Spanish</option>
             <option value="tr">Turkish</option>
           </select>
-          <input placeholder="Translate" />
+          <div className="output">{translation}</div>
         </div>
         <button>Submit</button>
       </form>
